551f4bf247c610badd538ffb4aea9f6726480fb1
Daniel Sousa <da.arada@gmail.com>
2012-06-28 22:39:59 +0100

    modified:   newprojectdialog.c

--- newprojectdialog.c
+++ newprojectdialog.c
@@ -8,31 +8,39 @@ enum
   N_COLUMNS
 };
 
+void
+set_column (GtkTreeViewColumn *column)
+{
+  gtk_tree_view_column_set_spacing (column, 5);
+  gtk_tree_view_column_set_resizable (column, TRUE);
+  gtk_tree_view_column_set_expand (column, TRUE);
+  gtk_tree_view_column_set_alignment (column, 0.5);
+  gtk_tree_view_column_set_reorderable (column, TRUE);
+}
+
 int
 newprojectdialog (GtkWidget *main_window)
 {
   GtkWidget *dialog;
   GtkWidget *content_area;
-  GtkWidget *project_frame_label;
-  GtkWidget *project_frame;
-  GtkWidget *project_grid;
-  GtkWidget *project_name_label;
-  GtkWidget *project_name;
-  GtkWidget *project_description_label;
-  GtkWidget *project_description_swin;
-  GtkWidget *project_description_view;
-  GtkTextBuffer *project_description;
-  GtkWidget *invoices_frame_label;
-  GtkWidget *invoices_frame;
-  GtkTreeStore *invoices_store;
-  GtkWidget *invoices_tree;
-  GtkTreeViewColumn *invoices_column;
-  GtkCellRenderer *invoices_renderer;
+  GtkWidget *label;
+  GtkWidget *frame;
+  GtkWidget *grid;
+  GtkWidget *name;
+  GtkWidget *scrollable_window;
+  GtkWidget *view;
+  GtkTextBuffer *description;
+  GtkTreeStore *orders;
+  GtkWidget *tree;
+  GtkTreeViewColumn *column;
+  GtkCellRenderer *renderer;
+  GtkTreeIter root_iter;
+  GtkTreeIter child_iter;
 
   dialog = gtk_dialog_new_with_buttons ("Creating a new Project...",
                                         GTK_WINDOW (main_window),
                                         GTK_DIALOG_DESTROY_WITH_PARENT,
-                                        GTK_STOCK_APPLY,
+                                        GTK_STOCK_ADD,
                                         GTK_RESPONSE_APPLY,
                                         GTK_STOCK_CANCEL,
                                         GTK_RESPONSE_CANCEL,
@@ -46,151 +54,120 @@ newprojectdialog (GtkWidget *main_window)
                                gdk_screen_height () * 1/2);
   gtk_container_set_border_width (GTK_CONTAINER (content_area), 10);
 
-  project_frame_label = gtk_label_new (NULL);
-  gtk_label_set_markup (GTK_LABEL (project_frame_label),
-                        "<span font_weight=\"bold\">Project</span>");
+  label = gtk_label_new (NULL);
+  gtk_label_set_markup (GTK_LABEL (label),
+                        "<b>Project</b>");
 
-  project_frame = gtk_frame_new (NULL);
-  gtk_frame_set_label_widget (GTK_FRAME (project_frame),
-                              project_frame_label);
+  frame = gtk_frame_new (NULL);
+  gtk_frame_set_label_widget (GTK_FRAME (frame),
+                              label);
   gtk_box_pack_start (GTK_BOX (content_area),
-                      project_frame,
+                      frame,
                       TRUE,
                       TRUE,
                       0);
 
-  project_grid = gtk_grid_new ();
-  gtk_grid_set_column_spacing (GTK_GRID (project_grid), 10);
-  gtk_grid_set_row_spacing (GTK_GRID (project_grid), 10);
-  gtk_container_set_border_width (GTK_CONTAINER (project_grid), 10);
-  gtk_container_add (GTK_CONTAINER (project_frame),
-                     project_grid);
+  grid = gtk_grid_new ();
+  gtk_grid_set_column_spacing (GTK_GRID (grid), 10);
+  gtk_grid_set_row_spacing (GTK_GRID (grid), 10);
+  gtk_container_set_border_width (GTK_CONTAINER (grid), 10);
+  gtk_container_add (GTK_CONTAINER (frame), grid);
 
-  project_name_label = gtk_label_new_with_mnemonic ("_Name");
-  gtk_widget_set_halign (project_name_label, GTK_ALIGN_END);
-  gtk_grid_attach_next_to (GTK_GRID (project_grid),
-                           project_name_label,
+  label = gtk_label_new_with_mnemonic ("_Name");
+  gtk_widget_set_halign (label, GTK_ALIGN_END);
+  gtk_grid_attach_next_to (GTK_GRID (grid),
+                           label,
                            NULL,
                            GTK_POS_TOP,
                            1, 1);
 
-  project_name = gtk_entry_new ();
-  gtk_entry_set_activates_default (GTK_ENTRY (project_name), TRUE);
-  gtk_label_set_mnemonic_widget (GTK_LABEL (project_name_label),
-                                 project_name);
-  gtk_widget_set_hexpand (project_name, TRUE);
-  gtk_grid_attach_next_to (GTK_GRID (project_grid),
-                           project_name,
-                           project_name_label,
+  name = gtk_entry_new ();
+  gtk_entry_set_activates_default (GTK_ENTRY (name), TRUE);
+  gtk_label_set_mnemonic_widget (GTK_LABEL (label), name);
+  gtk_widget_set_hexpand (name, TRUE);
+  gtk_grid_attach_next_to (GTK_GRID (grid),
+                           name,
+                           label,
                            GTK_POS_RIGHT,
                            1, 1);
 
-  project_description_label = gtk_label_new_with_mnemonic ("_Description");
-  gtk_widget_set_halign (project_description_label, GTK_ALIGN_END);
-  gtk_widget_set_valign (project_description_label, GTK_ALIGN_START);
-  gtk_grid_attach_next_to (GTK_GRID (project_grid),
-                           project_description_label,
+  label = gtk_label_new_with_mnemonic ("_Description");
+  gtk_widget_set_halign (label, GTK_ALIGN_END);
+  gtk_widget_set_valign (label, GTK_ALIGN_START);
+  gtk_grid_attach_next_to (GTK_GRID (grid),
+                           label,
                            NULL,
                            GTK_POS_BOTTOM,
                            1, 1);
 
-  project_description_swin = gtk_scrolled_window_new (NULL, NULL);
-  gtk_widget_set_hexpand (project_description_swin, TRUE);
-  gtk_widget_set_vexpand (project_description_swin, TRUE);
-  gtk_grid_attach_next_to (GTK_GRID (project_grid),
-                           project_description_swin,
-                           project_description_label,
+  scrollable_window = gtk_scrolled_window_new (NULL, NULL);
+  gtk_widget_set_hexpand (scrollable_window, TRUE);
+  gtk_widget_set_vexpand (scrollable_window, TRUE);
+  gtk_grid_attach_next_to (GTK_GRID (grid),
+                           scrollable_window,
+                           label,
                            GTK_POS_RIGHT,
                            1, 1);
 
-  project_description_view = gtk_text_view_new ();
-  gtk_label_set_mnemonic_widget (GTK_LABEL (project_description_label),
-                                 project_description_view);
-  gtk_widget_set_hexpand (project_description_view, TRUE);
-  gtk_container_add (GTK_CONTAINER (project_description_swin),
-                     project_description_view);
+  view = gtk_text_view_new ();
+  gtk_label_set_mnemonic_widget (GTK_LABEL (label), view);
+  gtk_widget_set_hexpand (view, TRUE);
+  gtk_container_add (GTK_CONTAINER (scrollable_window), view);
 
-  project_description = gtk_text_view_get_buffer (
-                          GTK_TEXT_VIEW (project_description_view));
+  description = gtk_text_view_get_buffer (
+                          GTK_TEXT_VIEW (view));
 
-  invoices_frame_label = gtk_label_new (NULL);
-  gtk_label_set_markup (GTK_LABEL (invoices_frame_label),
-                   "<span font_weight=\"bold\">Invoices and Payments</span>");
+  label = gtk_label_new (NULL);
+  gtk_label_set_markup (GTK_LABEL (label), "<b>Orders and Payments</b>");
 
-  invoices_frame = gtk_frame_new (NULL);
-  gtk_frame_set_label_widget (GTK_FRAME (invoices_frame),
-                              invoices_frame_label);
+  frame = gtk_frame_new (NULL);
+  gtk_frame_set_label_widget (GTK_FRAME (frame), label);
   gtk_box_pack_start (GTK_BOX (content_area),
-                      invoices_frame,
+                      frame,
                       TRUE,
                       TRUE,
                       0);
 
-  GtkTreeIter invoices_iter;
-  GtkTreeIter payments_iter;
+  orders = gtk_tree_store_new (N_COLUMNS,
+                               G_TYPE_STRING,
+                               G_TYPE_STRING,
+                               G_TYPE_DOUBLE);
+  gtk_tree_store_append (orders, &root_iter, NULL);
+  gtk_tree_store_append (orders, &child_iter, &root_iter);
 
-  invoices_store = gtk_tree_store_new (N_COLUMNS,
-                                       G_TYPE_STRING,
-                                       G_TYPE_STRING,
-                                       G_TYPE_DOUBLE);
-  gtk_tree_store_append (invoices_store, &invoices_iter, NULL);
-  gtk_tree_store_append (invoices_store, &payments_iter, &invoices_iter);
+  tree = gtk_tree_view_new_with_model (GTK_TREE_MODEL (orders));
 
-  invoices_tree = gtk_tree_view_new_with_model (GTK_TREE_MODEL (
-                                                invoices_store));
+  renderer = gtk_cell_renderer_text_new ();
+  g_object_set (G_OBJECT (renderer), "editable", TRUE, NULL);
+  column = gtk_tree_view_column_new_with_attributes ("Reference",
+                                                     renderer,
+                                                     "text",
+                                                     REFERENCE,
+                                                     NULL);
+  set_column (column);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
 
-  invoices_renderer = gtk_cell_renderer_text_new ();
-  g_object_set (G_OBJECT (invoices_renderer),
-                "editable", TRUE,
-                NULL);
-  invoices_column = gtk_tree_view_column_new_with_attributes (
-                      "Reference", invoices_renderer,
-                      "text", REFERENCE,
-                      NULL);
-  gtk_tree_view_column_set_spacing (invoices_column, 5);
-  gtk_tree_view_column_set_resizable (invoices_column, TRUE);
-  gtk_tree_view_column_set_expand (invoices_column, TRUE);
-  gtk_tree_view_column_set_alignment (invoices_column, 0.5);
-  gtk_tree_view_column_set_reorderable (invoices_column, TRUE);
-  gtk_tree_view_append_column (GTK_TREE_VIEW (invoices_tree),
-                               invoices_column);
+  renderer = gtk_cell_renderer_text_new ();
+  g_object_set (G_OBJECT (renderer), "editable", TRUE, NULL);
+  column = gtk_tree_view_column_new_with_attributes ("Description",
+                                                     renderer,
+                                                     "text",
+                                                     DESCRIPTION,
+                                                     NULL);
+  set_column (column);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
 
-  invoices_renderer = gtk_cell_renderer_text_new ();
-  g_object_set (G_OBJECT (invoices_renderer),
-                "editable", TRUE,
-                NULL);
-  invoices_column = gtk_tree_view_column_new_with_attributes (
-                      "Description", invoices_renderer,
-                      "text", DESCRIPTION,
-                      NULL);
-  gtk_tree_view_column_set_spacing (invoices_column, 5);
-  gtk_tree_view_column_set_resizable (invoices_column, TRUE);
-  gtk_tree_view_column_set_expand (invoices_column, TRUE);
-  gtk_tree_view_column_set_alignment (invoices_column, 0.5);
-  gtk_tree_view_column_set_reorderable (invoices_column, TRUE);
-  gtk_tree_view_append_column (GTK_TREE_VIEW (invoices_tree),
-                               invoices_column);
-  //gtk_tree_view_set_expander_column (GTK_TREE_VIEW (invoices_tree),
-                                     //invoices_column);
+  renderer = gtk_cell_renderer_text_new ();
+  g_object_set (G_OBJECT (renderer), "editable", TRUE, NULL);
+  column = gtk_tree_view_column_new_with_attributes ("Amount",
+                                                     renderer,
+                                                     "text",
+                                                     AMOUNT,
+                                                     NULL);
+  set_column (column);
+  gtk_tree_view_append_column (GTK_TREE_VIEW (tree), column);
 
-  invoices_renderer = gtk_cell_renderer_text_new ();
-  g_object_set (G_OBJECT (invoices_renderer),
-                "editable", TRUE,
-                NULL);
-  invoices_column = gtk_tree_view_column_new_with_attributes (
-                      "Amount", invoices_renderer,
-                      "text", AMOUNT,
-                      NULL);
-  gtk_tree_view_column_set_spacing (invoices_column, 5);
-  gtk_tree_view_column_set_resizable (invoices_column, TRUE);
-  gtk_tree_view_column_set_expand (invoices_column, TRUE);
-  gtk_tree_view_column_set_alignment (invoices_column, 0.5);
-  gtk_tree_view_column_set_reorderable (invoices_column, TRUE);
-  gtk_tree_view_append_column (GTK_TREE_VIEW (invoices_tree),
-                               invoices_column);
-
-  gtk_container_add (GTK_CONTAINER (invoices_frame),
-                     invoices_tree);
+  gtk_container_add (GTK_CONTAINER (frame), tree);
 
   gtk_widget_show_all (dialog);
 
@@ -198,7 +175,7 @@ newprojectdialog (GtkWidget *main_window)
 
   switch (result)
   {
-    case GTK_RESPONSE_APPLY:
+    case GTK_RESPONSE_OK:
       break;
     default:
       break;
